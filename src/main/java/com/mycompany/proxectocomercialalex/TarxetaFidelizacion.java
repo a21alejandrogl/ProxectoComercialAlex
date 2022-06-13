6399aabfb345cf230ba84ffb527ff91ae9a3a64f
dammdcd-a <dammdcd-a@a12eq34.sanclemente.local>
2022-06-13 16:38:03 +0200

    Versión inicial de ProxectoComercialAlex

---
+++ src/main/java/com/mycompany/proxectocomercialalex/TarxetaFidelizacion.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proxectocomercialalex;
+
+/**
+ *
+ * @author dammdcd-a
+ */
+public class TarxetaFidelizacion {
+    
+    private int puntos;
+
+    public TarxetaFidelizacion() {
+    }
+
+    public TarxetaFidelizacion(int puntosIniciais) {
+        this.puntos = puntosIniciais;
+    }
+    
+    public boolean obterPuntos(int valorCompra){
+        return true;
+    }
+    
+    public boolean abonarGaraxe(){
+        return true;
+    }
+    
+    public int obterRegalo(int numPuntosRegalo){
+        return numPuntosRegalo;
+    }
+
+    @Override
+    public String toString() {
+        return "TarxetaFidelizacion{" + "puntos=" + puntos + '}';
+    }
+    
+    
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    
+}
